aea5f34031abd59ae259891ffbc48085761cddc8
Carles Saumell <saumell19@gmail.com>
2023-03-03 15:22:17 +0100

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  
+
   public isOpen: boolean = false;
+  
   public isMobile: boolean = false;
 
   ngOnInit(): void {
