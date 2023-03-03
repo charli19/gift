fb470730c251176dae686137a400d7755173f469
Carles Saumell <saumell19@gmail.com>
2023-03-03 15:07:53 +0100

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'gift';
+export class AppComponent implements OnInit {
+  
+  public isOpen: boolean = false;
+  public isMobile: boolean = false;
+
+  ngOnInit(): void {
+    const userAgent = navigator.userAgent;
+
+    if (/iPhone/.test(userAgent)) {
+      this.isMobile = true;
+    }
+  }
+
+  openGift(): void {
+    this.isOpen = true;
+    const audio = new Audio('assets/super3.mp3');
+    audio.play();
+  }
 }
