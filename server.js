b80f8fe3f6239466819c126e9294480eee50ca48
Carles Saumell <saumellcarles@gmail.com>
2025-02-18 16:25:48 +0100

    deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.disable("x-powered-by");
 
-app.use(express.static(__dirname + "/dist/gift"));
+app.use(express.static(__dirname + "/dist/gift-noe/browser"));
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/gift/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/gift-noe/browser/index.html"));
 });
 
 app.listen(process.env.PORT || 4200);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public isOpen: boolean = false;
-  public isHidden: boolean = false;
-  public isMobile: boolean = false;
-
-  ngOnInit(): void {
-    const userAgent = navigator.userAgent;
-
-    if (/iPhone/.test(userAgent)) {
-      this.isMobile = true;
-    }
-  }
-
-  openGift(): void {
-    const audio = new Audio('assets/super3.mp3');
-    audio.play();
-    setInterval(() => {
-      this.isOpen = true;
-    }, 500);
-    setInterval(() => {
-      var el = document.getElementById('open');
-      if (el) {
-        el.style.display = 'none';
-      }
-    }, 1500);
-  }
+export class AppComponent {
+  title = 'gift-noe';
 }
